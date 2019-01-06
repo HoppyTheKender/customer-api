0358225436e8be4d3a826f7f9c966ad7521b4dd3
Jason Rueckert <jason.rueckert@outlook.com>
2019-01-06 16:29:47 -0600

    Removing dependencies to try to get the size of the output file down for deployment to GCP.

--- src/main/java/com/rueckert/customer/config/PcfRabbitMqConfig.java
+++ src/main/java/com/rueckert/customer/config/PcfRabbitMqConfig.java
@@ -1,7 +1,7 @@
 package com.rueckert.customer.config;
 
-import org.springframework.amqp.core.FanoutExchange;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+//import org.springframework.amqp.core.FanoutExchange;
+//import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +13,13 @@ public class PcfRabbitMqConfig extends AbstractCloudConfig {
 	public static final String CUSTOMER_TOPIC_NAME = "customer-topic";
 	public static final String CUSTOMER_QUEUE_NAME = "customer-email-queue";
 
-	@Bean
-	public FanoutExchange fanoutExchange() {
-		return new FanoutExchange(CUSTOMER_TOPIC_NAME, true, false);
-	}
-
-	@Bean
-	public RabbitAdmin rabbitAdmin() {
-		return new RabbitAdmin(connectionFactory().rabbitConnectionFactory());
-	}
+//	@Bean
+//	public FanoutExchange fanoutExchange() {
+//		return new FanoutExchange(CUSTOMER_TOPIC_NAME, true, false);
+//	}
+//
+//	@Bean
+//	public RabbitAdmin rabbitAdmin() {
+//		return new RabbitAdmin(connectionFactory().rabbitConnectionFactory());
+//	}
 }
--- src/main/java/com/rueckert/customer/notifier/CustomerChangeNotifierRabbitMqImpl.java
+++ src/main/java/com/rueckert/customer/notifier/CustomerChangeNotifierRabbitMqImpl.java
@@ -2,31 +2,31 @@ package com.rueckert.customer.notifier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpException;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.AmqpException;
+//import org.springframework.amqp.core.Exchange;
+//import org.springframework.amqp.rabbit.core.RabbitAdmin;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import com.rueckert.customer.config.PcfRabbitMqConfig;
 
 public class CustomerChangeNotifierRabbitMqImpl implements CustomerChangeNotifier {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private RabbitTemplate rabbitTemplate;
+//	private RabbitTemplate rabbitTemplate;
 
-	public CustomerChangeNotifierRabbitMqImpl(RabbitAdmin rabbitAdmin, Exchange exchange) {
-		rabbitAdmin.declareExchange(exchange);
-		this.rabbitTemplate = rabbitAdmin.getRabbitTemplate();
-
-		rabbitTemplate.setExchange(exchange.getName());
-	}
+//	public CustomerChangeNotifierRabbitMqImpl(RabbitAdmin rabbitAdmin, Exchange exchange) {
+//		rabbitAdmin.declareExchange(exchange);
+//		this.rabbitTemplate = rabbitAdmin.getRabbitTemplate();
+//
+//		rabbitTemplate.setExchange(exchange.getName());
+//	}
 
 	@Override
 	public void sendNotification(String id) {
-		try {
-			rabbitTemplate.convertAndSend(PcfRabbitMqConfig.CUSTOMER_TOPIC_NAME, null, id);
-		} catch (AmqpException e) {
-			logger.error("An exception occurred trying to publish a message.", e);
-		}
+//		try {
+//			rabbitTemplate.convertAndSend(PcfRabbitMqConfig.CUSTOMER_TOPIC_NAME, null, id);
+//		} catch (AmqpException e) {
+//			logger.error("An exception occurred trying to publish a message.", e);
+//		}
 	}
 }
